b19f5c3a5734941debc7d09b51bbd4da5b51d1a6
João Paulo Dias <joaopdiasventura@gmail.com>
2024-02-22 11:17:44 -0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MULTI_PAGE_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AboutBox1 np = new AboutBox1();
+            np.Show();
+            this.Hide();
+        }
+    }
+}
